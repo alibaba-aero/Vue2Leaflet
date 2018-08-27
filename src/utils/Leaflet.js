bd8b93a43c9e6111379c3c1906eddf5a332e9b84
Sasan Farrokh <s.farrokh@alibaba.ir>
2018-08-27 10:27:18 +0430

    fix: Reverted computed prop to method

--- src/utils/Leaflet.js
+++ src/utils/Leaflet.js
@@ -7,12 +7,9 @@ const isBrowser = typeof window !== 'undefined'
 let $leaflet = isBrowser ? window.L : null;
 
 export const LeafletMixin = {
-
-    computed: {
-        $leaflet: () => $leaflet,
-    },
-
     methods: {
+        $leaflet: () => $leaflet,
+
         $loadLeaflet: () => {
             if (isBrowser && !$leaflet) {
                 $leaflet = require('leaflet');
