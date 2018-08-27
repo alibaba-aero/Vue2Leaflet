3e0d70aed6f2df30ef8fc915b4acd412f085e510
Pooya Parsa <pyapar@gmail.com>
2018-08-27 10:45:39 +0430

    Update Leaflet.js

--- src/utils/Leaflet.js
+++ src/utils/Leaflet.js
@@ -9,7 +9,6 @@ let $leaflet = isBrowser ? window.L : null;
 export const LeafletMixin = {
     methods: {
         $leaflet: () => $leaflet,
-
         $loadLeaflet: () => {
             if (isBrowser && !$leaflet) {
                 $leaflet = require('leaflet');
